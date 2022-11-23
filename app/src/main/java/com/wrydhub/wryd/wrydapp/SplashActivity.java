4f0371cf5534a5476fa50ba046294a33ae444e43
Yuvraj Singh <yuvraj0157@gmail.com>
2022-11-23 23:01:00 +0530

    Added splash screen

---
+++ app/src/main/java/com/wrydhub/wryd/wrydapp/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.wrydhub.wryd.wrydapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent iHome = new Intent(SplashActivity.this,Login.class);
+                startActivity(iHome);
+                finish();
+            }
+        }, 3000);
+    }
+}
